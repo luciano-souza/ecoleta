393fecc9ee7f9b91b007b13f2810cccebff61bae
Luciano Souza <luciano.souza@ifnmg.edu.br>
2020-06-02 12:11:31 -0300

    Server: Listando os items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
-  response.json({ message: "Hello Luciano"});
+routes.get('/items', async (request, response) => {
+
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  });
+
+  response.json(serializedItems);
 });
 
 export default routes;
